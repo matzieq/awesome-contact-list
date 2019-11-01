b69bd8c690bd2349434b282b48e91afcb1a72f2b
Maciej Kozlowski <mjp.kozlowski@gmail.com>
2019-11-01 10:04:54 +0100

    Add messages for empty lists

--- src/Components/TagInfo/index.tsx
+++ src/Components/TagInfo/index.tsx
@@ -1,25 +1,33 @@
-import React, { useContext } from 'react';
-import { ListItem, ListItemLabel } from 'baseui/list';
-import { Context } from '../StateProvider';
-import { Button } from 'baseui/button';
+import React, { useContext } from "react";
+import { ListItem, ListItemLabel } from "baseui/list";
+import { Context } from "../StateProvider";
+import { Button } from "baseui/button";
 
 const TagInfo = () => {
   const { tags, setTags } = useContext(Context);
   return (
     <ul>
-      {tags.map((tag: any) => (
-        <ListItem key={tag.id}>
-          <ListItemLabel>{tag.name}</ListItemLabel>
-          <Button>Edit</Button>
-          <Button onClick={() =>{
-            const editedTags = tags.filter(
-              (currentTag: any) => currentTag.id !== tag.id
-            );
-            console.log(editedTags);
-            setTags(editedTags);
-          }}>Delete</Button>
-        </ListItem>
-      ))}
+      {tags.length ? (
+        tags.map((tag: any) => (
+          <ListItem key={tag.id}>
+            <ListItemLabel>{tag.name}</ListItemLabel>
+            <Button>Edit</Button>
+            <Button
+              onClick={() => {
+                const editedTags = tags.filter(
+                  (currentTag: any) => currentTag.id !== tag.id
+                );
+                console.log(editedTags);
+                setTags(editedTags);
+              }}
+            >
+              Delete
+            </Button>
+          </ListItem>
+        ))
+      ) : (
+        <ListItem>Much empty...</ListItem>
+      )}
     </ul>
   );
 };
