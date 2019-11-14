e221c54c96af7677c39a30f7822bc4e4bd5471b5
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2019-11-14 16:02:36 +0100

    Add skill selection

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { LightTheme, BaseProvider, styled } from 'baseui';
-import { StateProvider } from './Components/StateProvider';
-import MainView from './Components/MainView';
+import { StateProvider } from './components/StateProvider';
+import MainView from './components/MainView';
 
 const engine = new Styletron();
 
